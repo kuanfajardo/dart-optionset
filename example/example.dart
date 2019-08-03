2f094298955b7596afe299288e6effa7686948d8
Juan Diego Fajardo <jfajardo@mit.edu>
2019-08-03 22:47:05 +0300

    Add basic example.

---
+++ example/example.dart
@@ -0,0 +1,21 @@
+import 'package:option_set/option_set.dart';
+
+class ImageFormat extends OptionSet<ImageFormat> {
+  const ImageFormat._(rawValue) : super(rawValue);
+
+  // OPTIONS //
+  static final png = const ImageFormat._(1 << 0);
+  static final jpeg = const ImageFormat._(1 << 1);
+  static final svg = const ImageFormat._(1 << 2);
+  static final gif = const ImageFormat._(1 << 3);
+
+  static final master = png & jpeg;
+
+  @override
+  final List<String> optionNames = const ['png', 'jpeg', 'svg', 'gif'];
+
+  @override
+  ImageFormat initWithRawValue(int rawValue) {
+    return ImageFormat._(rawValue);
+  }
+}
